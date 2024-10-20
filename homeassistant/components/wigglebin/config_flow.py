5844a1ed851a1b74875a3081bbd108c12ea7b8bb
Vincent Kranendonk <git@studiorabota.com>
2024-10-20 10:34:15 +0000

    Added input fields to OptionsFlow as well

--- homeassistant/components/wigglebin/config_flow.py
+++ homeassistant/components/wigglebin/config_flow.py
@@ -59,10 +59,23 @@ class WiggleBinOptionsFlowHandler(OptionsFlow):
 
     async def async_step_user(self, user_input=None) -> ConfigFlowResult:
         """Handle the initial step."""
+        errors: dict = {}
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
+        current_data = self.config_entry.data
+
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({}),
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        "name", default=current_data.get("name", DEFAULT_NAME)
+                    ): str,
+                    vol.Required(
+                        "api_url", default=current_data.get("api_url", DEFAULT_API_URL)
+                    ): str,
+                }
+            ),
+            errors=errors,
         )
