dbc5b164b356cb22c6655534c4d1cfcd3bf96af4
Vincent Kranendonk <git@studiorabota.com>
2024-10-20 10:01:56 +0000

    Added sensor device class so it can be used as a statistic

--- homeassistant/components/wigglebin/sensor.py
+++ homeassistant/components/wigglebin/sensor.py
@@ -3,7 +3,11 @@
 import logging
 from typing import Any
 
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -22,15 +26,17 @@ async def async_setup_entry(
     sensors = [
         WiggleBinSensor(
             coordinator,
-            "wiggle_air_temperature",
-            "WiggleBin Air Temperature",
+            "wigglebin_temperature",
+            "WiggleBin Temperature",
             "temperature",
+            SensorDeviceClass.TEMPERATURE,
         ),
         WiggleBinSensor(
             coordinator,
-            "wiggle_air_humidity",
-            "WiggleBin Air Humidity",
+            "wigglebin_humidity",
+            "WiggleBin Humidity",
             "humidity",
+            SensorDeviceClass.HUMIDITY,
         ),
         # Add more sensors as needed
     ]
@@ -41,12 +47,15 @@ async def async_setup_entry(
 class WiggleBinSensor(SensorEntity):
     """Representation of a Sensor."""
 
-    def __init__(self, coordinator, sensor_id, name, attribute) -> None:
+    def __init__(
+        self, coordinator, sensor_id, name, attribute, device_class: SensorDeviceClass
+    ) -> None:
         """Initialize the sensor."""
         self.coordinator = coordinator
         self._sensor_id = sensor_id
         self._name = name
         self._attribute = attribute
+        self._device_class = device_class
 
     @property
     def name(self) -> str:
@@ -77,6 +86,16 @@ class WiggleBinSensor(SensorEntity):
     def available(self) -> bool:
         """Return if the sensor is available."""
         return self.coordinator.last_update_success
+
+    @property
+    def device_class(self) -> SensorDeviceClass:
+        """Return the device class of the sensor."""
+        return self._device_class
+
+    @property
+    def state_class(self) -> SensorStateClass:
+        """Return the state class of the sensor."""
+        return SensorStateClass.MEASUREMENT
 
     async def async_update(self) -> None:
         """Update the sensor."""
